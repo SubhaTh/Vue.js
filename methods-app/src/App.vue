707ace03b831892ad672a37f1b7740088038f8d8
SubhaTh <subha.thirumugam@p360.com>
2023-04-20 13:24:52 -0400

    input v-model directives

--- methods-app/src/App.vue
+++ methods-app/src/App.vue
@@ -3,11 +3,18 @@
     <h1>Hello world</h1>
     <!-- <img src="./assets/logo.png" alt=""> -->
     <img src="@/assets/logo.png" alt="">
+    <input type="text" v-model="inputValue">
+    <h1>{{ inputValue }}</h1>
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      inputValue: '',
+    };
+  }
 }
 </script>
 <style>
